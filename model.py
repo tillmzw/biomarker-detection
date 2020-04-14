89ff57e9e87b50156ec97faa87b994e986e6455e
Till Meyer zu Westram <till@meyerzuwestram.net>
2020-04-14 15:05:36 +0200

    replace sigmoid with softmax as model output; sigmoid already part of loss fn!

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
-        x = torch.sigmoid(x)
+        x = F.softmax(x)
         return x
 
 
